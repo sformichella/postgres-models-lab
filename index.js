adccd1ea6ae4f374ecf6d313177b90d1a3f39e45
sformichella <sformichella@gmail.com>
2020-12-07 17:00:14 -0800

    set up other models

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const Theorem = require('./lib/models/theorem')
+const Theorem = require('./lib/models/theorem');
+const Mathematician = require('./lib/models/mathematician');
+const ProofTechnique = require('./lib/models/proofTechnique');
+const FamousNumber = require('./lib/models/famousNumber');
+const Paradox = require('./lib/models/paradox');
 
 app.use(express.json());
 const port = 7890;
 
-app.post('/make/theorem', async (req, res) => {
-  Theorem.insert(req.body)
-    .then(thm => res.send(thm))
+
+// Theorem Routes
+app.get('/theorems', (req, res) => {
+  Theorem.getAll()
+    .then(thms => res.send(thms));
 })
+
+app.get('/theorems/:id', (req, res) => {
+  Theorem.getById(req.query.id)
+    .then(thm => res.send(thm));
+})
+
+app.post('/make/theorem', (req, res) => {
+  Theorem.insert(req.body)
+    .then(thm => res.send(thm));
+})
+
+
 
 app.listen(port, () => {
   console.log(`Started on ${port}.`);
